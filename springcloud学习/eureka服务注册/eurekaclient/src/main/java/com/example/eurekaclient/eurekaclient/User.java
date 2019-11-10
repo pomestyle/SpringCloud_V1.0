71ac3c44a8db0c9e515706b6dc34e1c95f08fc9b
pomestyle <43631117+pomestyle@users.noreply.github.com>
2019-11-10 16:19:12 +0800

    eureka注册中心

---
+++ springcloud学习/eureka服务注册/eurekaclient/src/main/java/com/example/eurekaclient/eurekaclient/User.java
@@ -0,0 +1,56 @@
+package com.example.eurekaclient.eurekaclient;
+
+/**
+ * @description: user
+ * @author: Administrator
+ * @create: 2019-11-06 22:34
+ **/
+public class User {
+
+    private int id;
+    private String name;
+    private String sex;
+    private String count;
+
+    public User() {
+    }
+
+    public User(int id, String name, String sex, String count) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.count = count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+}
