28c7b7e60fe9dc008ca2ad63ba045bdbbfeb2956
pomestyle <43631117+pomestyle@users.noreply.github.com>
2019-11-16 15:54:54 +0800

    eureka 服务注册到 eureka 集群中

---
+++ eureka服务注册/regstertoeureka/src/test/java/com/example/regstertoeureka/regstertoeureka/RegstertoeurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.regstertoeureka.regstertoeureka;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegstertoeurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
