08dc323092bc452d27be000458d9cc0a6264074d
pomestyle <43631117+pomestyle@users.noreply.github.com>
2019-12-07 18:31:37 +0800

    添加远程配置文件  以及使用eureka 作为配置中心

--- spring cloud-config/springcloud-client-configserver/src/main/java/com/example/springcloudconfigserver/SpringcloudconfigserverApplication.java
+++ spring cloud-config/springcloud-client-configserver/src/main/java/com/example/springcloudconfigserver/SpringcloudconfigserverApplication.java
@@ -1,15 +1,22 @@
-package com.example.springcloudconfigclient;
+package com.example.springcloudconfigserver;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
-public class SpringcloudconfigclientApplication {
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
+public class SpringcloudconfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudconfigserverApplication.class, args);
+    }
 
     @Value("${password}")
     private String password;
@@ -20,9 +27,5 @@ public class SpringcloudconfigclientApplication {
         return this.password;
     }
 
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringcloudconfigclientApplication.class, args);
-    }
 
 }
--- spring cloud-config/springcloud-eureka-configserver/src/main/java/com/example/springcloudconfigserver/SpringcloudconfigserverApplication.java
+++ spring cloud-config/springcloud-eureka-configserver/src/main/java/com/example/springcloudconfigserver/SpringcloudconfigserverApplication.java
@@ -3,9 +3,11 @@ package com.example.springcloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //启用configServer
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringcloudconfigserverApplication {
 
